e586e6a1a9c7281744174aaedfbe8f02ff9455ea
Tynnikov <49746547+Tynnikov@users.noreply.github.com>
2020-08-18 22:52:47 +0500

    Update PyLesson 6.py

--- PyLesson 6.py
+++ PyLesson 6.py
@@ -168,4 +168,38 @@ town_car.stop()
 town_car.turn('right')
 town_car.show_speed()
 
- 
+ class Stationery():
+    def __init__(self, title='Brand name'):
+        self.title = title
+
+    def draw(self):
+        print("Запуск отрисовки")
+
+
+''' Задание - 5:
+Реализовать класс Stationery (канцелярская принадлежность). Определить в нем атрибут title (название) и
+метод draw (отрисовка). Метод выводит сообщение “Запуск отрисовки.” Создать три дочерних класса Pen (ручка),
+Pencil (карандаш), Handle (маркер). В каждом из классов реализовать переопределение метода draw.
+Для каждого из классов метод должен выводить уникальное сообщение. Создать экземпляры классов и проверить,
+что выведет описанный метод для каждого экземпляра.
+'''        
+        
+class Pen(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки с помощью {self.title} Pen")
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки с помощью {self.title} Pencil")
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки с помощью {self.title} Handle")
+
+
+pen = Pen('Zabuza')
+pen.draw()
+pencil = Pencil('Cisco')
+pencil.draw()
+handle = Handle('Marker')
+handle.draw()
